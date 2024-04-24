26eeea336d54e231d5c7fdbde0414ad9ecad8cae
GitNinja749 <vivekpp749@gmail.com>
2024-04-24 07:38:54 +0530

    Second commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,12 +14,13 @@ const Home = () => {
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={Photos} alt=""  className='w-60 h-60 md:w-[400px] md:h-[400px] rounded-lg max-w-sm  shadow-2xl' data-aos="zoom-in-up"/>
           <div>
-            <h1 className='text-lg md:text-xl'>Hi there, I’m </h1>
+            <h1 className='text-lg md:text-xl animate-bounce'>Hi</h1>
+            <h1 className='text-lg md:text-xl'>there, I’m </h1>
             <h1 className="text-2xl md:text-5xl font-bold"><span  className='hover:text-transparent bg-gradient-to-r from-blue-600 via-purple-600 to-yellow-300 bg-clip-text'>
                Vivek</span> a <span  className='text-transparent bg-gradient-to-r from-blue-600 via-purple-600 to-yellow-300 bg-clip-text'>
                Web Developer</span> </h1>
             <p className="py-3 md:py-6">I am highly skilled MERN stack developer.Committed to delivering high-quality code and ensuring optimal performance. Seeking a challenging position where I can utilize my skills and contribute to the success of a dynamic organization.</p>
-            <button className="btn btn-primary">Download CV</button>
+            <button className="btn btn-primary ">Download CV</button>
           </div>
         </div>
       </div>
