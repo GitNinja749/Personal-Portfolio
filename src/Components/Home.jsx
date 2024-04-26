c36eb1d9bdad73a9033a00ef49eaa7a5fdb5bf86
GitNinja749 <vivekpp749@gmail.com>
2024-04-26 14:20:11 +0530

    Overflow

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,10 +9,9 @@ const Home = () => {
       <div className='dark:text-white dark:bg-gray-950 pt-12'>
         {/* NavBar */}
         <NavBar/>
-       
        <div className="hero min-h-screen bg-base-200 dark:text-white dark:bg-gray-950">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src={Photos} alt=""  className='w-60 h-60 md:w-[400px] md:h-[400px] rounded-lg max-w-sm  shadow-2xl brightness-90 dark:brightness-75' data-aos="zoom-in-up"/>
+          <img src={Photos} alt=""  className='w-60 h-60 md:w-[400px] md:h-[400px] rounded-lg max-w-sm  shadow-2xl brightness-90 dark:brightness-75'/>
           <div>
             <h1 className='text-lg md:text-xl animate-bounce'>Hi</h1>
             <h1 className='text-lg md:text-xl'>there, I’m </h1>
